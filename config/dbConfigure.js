2d8410e02cf9c46d808936f048b8cd1deda65338
RaiShivaniiii <Shivani.Rai@cylsys.com>
2024-05-22 16:54:50 +0530

    first commit

---
+++ config/dbConfigure.js
@@ -0,0 +1,18 @@
+
+'user strict';
+ 
+const mysql = require('mysql2');
+ 
+//local mysql db connection
+const dbConn = mysql.createConnection({
+  
+  host: "103.228.83.115",
+  user: "Kodiemysql", // USER NAME
+  database: "kodie", // DATABASE NAME
+  password: "Cylsys@Kodie@2",
+});
+dbConn.connect(function(err) {
+  if (err) throw err;
+  console.log("Database Connected!");
+});
+module.exports = dbConn;
